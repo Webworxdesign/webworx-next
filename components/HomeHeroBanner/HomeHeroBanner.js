f2b4f747e75f31b4d60664e7678da18d14993190
Calvin <32037390+CalvinsGit@users.noreply.github.com>
2023-12-14 15:59:37 +0200

    removed margin from mix deifference settings

--- components/HomeHeroBanner/HomeHeroBanner.js
+++ components/HomeHeroBanner/HomeHeroBanner.js
@@ -77,6 +77,7 @@ export default function HomeHeroBanner() {
         )
 
         const bannerDescAnime = bannerDescRef.current;
+        console.log('bannerb',bannerDescAnime);
         gsap.fromTo(
             bannerDescAnime, 
             { 
