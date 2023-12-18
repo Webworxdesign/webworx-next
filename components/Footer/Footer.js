04159db156a93ae1afdee2715258d6d5eb9af495
Calvin <32037390+CalvinsGit@users.noreply.github.com>
2023-12-18 11:05:06 +0200

    Footer hover changes & social icons color invert for dark mode

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -63,7 +63,7 @@ export default function Footer({ title, menuItems }) {
         repeat: 0,
         rotationX: -120,
         transformOrigin: "right top",
-        y: 80,
+        y: 90,
         skewX: "7deg"
       })
     } 
@@ -112,24 +112,54 @@ export default function Footer({ title, menuItems }) {
           ele.removeEventListener("mouseleave", mouseLeave)          
         }
       })
-    }, 2000)
+    }, 6000)
+
+    setTimeout(() => {      
+      const element = [buttonOuter.current];
+      element.push(...document.querySelectorAll('.footer-logo'))
+
+      element.forEach((ele)=>{
+        const handleMouseMove = (event) => {
+          const { clientX, clientY } = event;
+          const { left, top, width, height } = ele.getBoundingClientRect();
+    
+          const xPercent = ((clientX - left) / width - 0.5) // Normalize mouse position
+          const yPercent = ((clientY - top) / height - 0.5) * 4
+          console.log(yPercent,xPercent);
+          gsap.to(ele, {
+            duration: 0.5,
+            rotationX: 10 * yPercent, // Adjust the tilt sensitivity by changing the multiplier
+            rotationY: 10 * xPercent,
+            transformPerspective: 500,
+            ease: 'power2.out',
+          });
+        };
+    
+        const handleMouseLeave = () => {
+          gsap.to(ele, {
+            duration: 0.5,
+            rotationX: 0,
+            rotationY: 0,
+            ease: 'power2.out',
+          });
+        };
+    
+        ele.addEventListener('mousemove', handleMouseMove);
+        ele.addEventListener('mouseleave', handleMouseLeave);
+    
+        return () => {
+          ele.removeEventListener('mousemove', handleMouseMove);
+          ele.removeEventListener('mouseleave', handleMouseLeave);
+        };
+      })
+    }, 6000);
 
   },[])
-  
-  const buttonHover = (e) => {
-    const bounding = e.target.getBoundingClientRect()
-    setButtonHoverPos({
-      // x:Math.round(((e.clientX/e.pageX)-0.5 + Number.EPSILON) * 10) / 10,
-      // y:Math.round(((e.clientY/e.pageY)-0.5 + Number.EPSILON) * 10) / 10
-      x:(Math.round(bounding.x + Number.EPSILON))/10,
-      y:(Math.round(bounding.y + Number.EPSILON))/10
-    })
-    // console.log('happening',buttonOuter.current.getBoundingClientRect() );
-  }
+
 
   return (
     <footer className={cx('component')} >
-      <div className="button-outer" ref={buttonOuter} onMouseOver={(e)=>{buttonHover(e)}} >
+      <div className="button-outer" ref={buttonOuter} >
         <button className="btn btn-primary marquee" onMouseEnter={() => {setHoverState(true)}} onMouseLeave={() => {setHoverState(false)}} ref={projectButton} >
             <div className="marquee__inner" ref={buttonTrack} >
                 <span className="marquee__line">Start a project <ProjectArrow /></span>
